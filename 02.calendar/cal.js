95bf62ed4339abdbdc8dc0d5a64f956566ac68b3
sharoa119 <sau.rin1109@gmail.com>
2024-06-10 16:41:29 +0900

    不要な変数を削除したり、while文をfor文に変更など。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,37 +3,31 @@
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-const yearArg = args.y;
-const monthArg = args.m;
 
 const today = new Date();
-const year = yearArg ?? today.getFullYear();
-const month = monthArg ?? today.getMonth() + 1;
+const year = args.y ?? today.getFullYear();
+const month = args.m ?? today.getMonth() + 1;
 
 const firstDay = new Date(year, month - 1);
 const lastDay = new Date(year, month, 0);
 
 console.log(`${" ".repeat(6)}${month}月 ${year}`);
 
-const dayNames = ["日", "月", "火", "水", "木", "金", "土"];
-console.log(dayNames.join(" "));
+console.log("日 月 火 水 木 金 土");
 
 process.stdout.write("   ".repeat(firstDay.getDay()));
 
-let currentDate = firstDay;
+for (
+  let currentDate = new Date(firstDay);
+  currentDate <= lastDay;
+  currentDate.setDate(currentDate.getDate() + 1)
+) {
+  process.stdout.write(currentDate.getDate().toString().padStart(2, " "));
 
-while (currentDate <= lastDay) {
-  const day = currentDate.getDate().toString().padStart(2, " ");
-  const dayOfWeek = currentDate.getDay();
-
-  process.stdout.write(day);
-
-  if (dayOfWeek === 6) {
+  if (currentDate.getDay() === 6) {
     process.stdout.write("\n");
-  } else {
+  } else if (currentDate < lastDay) {
     process.stdout.write(" ");
   }
-
-  currentDate.setDate(currentDate.getDate() + 1);
 }
 console.log();
