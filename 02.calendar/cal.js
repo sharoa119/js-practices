8a2067b759feb28a9bdfe7477fa3c29f1cc35f59
sharoa119 <sau.rin1109@gmail.com>
2024-06-12 10:36:24 +0900

    いらない空行を削除。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,7 +3,6 @@
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-
 const today = new Date();
 const year = args.y ?? today.getFullYear();
 const month = args.m ?? today.getMonth() + 1;
@@ -12,9 +11,7 @@ const firstDay = new Date(year, month - 1);
 const lastDay = new Date(year, month, 0);
 
 console.log(`${" ".repeat(6)}${month}月 ${year}`);
-
 console.log("日 月 火 水 木 金 土");
-
 process.stdout.write("   ".repeat(firstDay.getDay()));
 
 for (
