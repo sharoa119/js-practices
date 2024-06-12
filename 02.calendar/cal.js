d878eb6619aeba1909652df7395d2b1dd91ee46d
sharoa119 <sau.rin1109@gmail.com>
2024-06-12 10:52:52 +0900

    変数名を変更。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,23 +7,23 @@ const today = new Date();
 const year = args.y ?? today.getFullYear();
 const month = args.m ?? today.getMonth() + 1;
 
-const firstDay = new Date(year, month - 1);
-const lastDay = new Date(year, month, 0);
+const firstDate = new Date(year, month - 1);
+const lastDate = new Date(year, month, 0);
 
 console.log(`${" ".repeat(6)}${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(firstDay.getDay()));
+process.stdout.write("   ".repeat(firstDate.getDay()));
 
 for (
-  let currentDate = new Date(firstDay);
-  currentDate <= lastDay;
+  let currentDate = new Date(firstDate);
+  currentDate <= lastDate;
   currentDate.setDate(currentDate.getDate() + 1)
 ) {
   process.stdout.write(currentDate.getDate().toString().padStart(2, " "));
 
   if (currentDate.getDay() === 6) {
     process.stdout.write("\n");
-  } else if (currentDate < lastDay) {
+  } else if (currentDate < lastDate) {
     process.stdout.write(" ");
   }
 }
