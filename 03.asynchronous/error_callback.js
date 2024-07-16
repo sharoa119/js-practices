63e01cc46319736ceda1abb327bd7bcb4725f30c
sharoa119 <sau.rin1109@gmail.com>
2024-07-16 15:22:47 +0900

    コールバックのエラーありを作成

--- 03.asynchronous/error_callback.js
+++ 03.asynchronous/error_callback.js
@@ -1,43 +1,50 @@
 #!/usr/bin/env node
 
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
 
-// インメモリデータベースを作成
-const db = new sqlite3.Database(':memory:');
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    insertData();
+  },
+);
 
-// booksテーブルを作成
-db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", () => {
-  insertData();
-});
-
-// データを挿入する関数
 function insertData() {
-  // データを挿入
-  db.run("INSERT INTO books(title) VALUES('cherry')", function() {
+  db.run("INSERT INTO books(title) VALUES('cherry')", function () {
     console.log(this.lastID);
 
-    db.run("INSERT INTO non_existing_table(title) VALUES('blueberry')", function(err) {
-      if (err) {
-        console.error(err.message);
-      } else {
+    db.run(
+      "INSERT INTO non_existing_table(title) VALUES('blueberry')",
+      function (err) {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
         console.log(this.lastID);
-      }
 
-      // データを読み取る
-      readData();
-    });
+        readData();
+      },
+    );
   });
 }
-  // データを読み取る
-function readData() {
-  db.each("SELECT * FROM non_existing_table", (err, row) => {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    console.log(row);
-  });
 
-  // データベース接続をクローズ
+function readData() {
+  db.each(
+    "SELECT * FROM non_existing_table",
+    (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log(row);
+    },
+    () => {
+      closeDatabase();
+    },
+  );
+}
+
+function closeDatabase() {
   db.close();
 }
