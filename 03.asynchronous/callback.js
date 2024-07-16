f65297a97092b4ba1be20b6b6fd2825b69b2c9aa
sharoa119 <sau.rin1109@gmail.com>
2024-07-16 15:21:47 +0900

    コールバックのエラーなしを作成

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,35 +1,39 @@
 #!/usr/bin/env node
 
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
 
-// インメモリデータベースを作成
-const db = new sqlite3.Database(':memory:');
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    insertData();
+  },
+);
 
-// booksテーブルを作成
-db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", () => {
-  insertData();
-});
-
-// データを挿入する関数
 function insertData() {
-  // データを挿入
-  db.run("INSERT INTO books(title) VALUES('cherry')", function() {
+  db.run("INSERT INTO books(title) VALUES('cherry')", function () {
     console.log(this.lastID);
 
-    db.run("INSERT INTO books(title) VALUES('blueberry')", function() {
+    db.run("INSERT INTO books(title) VALUES('blueberry')", function () {
       console.log(this.lastID);
 
-      // データを読み取る
       readData();
     });
   });
 }
-  // データを読み取る
-function readData() {
-  db.each("SELECT * FROM books", (err, row) => {
-    console.log(row);
-  });
 
-  // データベース接続をクローズ
+function readData() {
+  db.each(
+    "SELECT * FROM books",
+    (err, row) => {
+      console.log(row);
+    },
+    () => {
+      closeDatabase();
+    },
+  );
+}
+
+function closeDatabase() {
   db.close();
 }
