771a5e4de18d762bdcfeb786738768a32e39d429
sharoa119 <sau.rin1109@gmail.com>
2024-07-18 17:09:37 +0900

    コールバックのエラーありとなしを見直しました。

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -5,35 +5,23 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  () => {
-    insertData();
-  },
-);
-
-function insertData() {
-  db.run("INSERT INTO books(title) VALUES('cherry')", function () {
-    console.log(this.lastID);
-
-    db.run("INSERT INTO books(title) VALUES('blueberry')", function () {
+  function () {
+    db.run("INSERT INTO books(title) VALUES('cherry')", function () {
       console.log(this.lastID);
 
-      readData();
+      db.run("INSERT INTO books(title) VALUES('blueberry')", function () {
+        console.log(this.lastID);
+
+        db.all("SELECT * FROM books", function (err, rows) {
+          rows.forEach((row) => {
+            console.log(row);
+          });
+
+          db.run("DROP TABLE books", function () {
+            db.close();
+          });
+        });
+      });
     });
-  });
-}
-
-function readData() {
-  db.each(
-    "SELECT * FROM books",
-    (err, row) => {
-      console.log(row);
-    },
-    () => {
-      closeDatabase();
-    },
-  );
-}
-
-function closeDatabase() {
-  db.close();
-}
+  },
+);
--- 03.asynchronous/error_callback.js
+++ 03.asynchronous/error_callback.js
@@ -5,46 +5,24 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  () => {
-    insertData();
-  },
-);
-
-function insertData() {
-  db.run("INSERT INTO books(title) VALUES('cherry')", function () {
-    console.log(this.lastID);
-
+  function () {
     db.run(
-      "INSERT INTO non_existing_table(title) VALUES('blueberry')",
+      "INSERT INTO non_existing_table(title) VALUES('cherry')",
       function (err) {
         if (err) {
           console.error(err.message);
-          return;
         }
-        console.log(this.lastID);
 
-        readData();
+        db.all("SELECT * FROM non_existing_table", function (err) {
+          if (err) {
+            console.error(err.message);
+          }
+
+          db.run("DROP TABLE books", function () {
+            db.close();
+          });
+        });
       },
     );
-  });
-}
-
-function readData() {
-  db.each(
-    "SELECT * FROM non_existing_table",
-    (err, row) => {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      console.log(row);
-    },
-    () => {
-      closeDatabase();
-    },
-  );
-}
-
-function closeDatabase() {
-  db.close();
-}
+  },
+);
