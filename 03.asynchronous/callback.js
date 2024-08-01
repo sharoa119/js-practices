ead8a91c080d4fbaab0865e9e43efdc8214fd8f1
sharoa119 <sau.rin1109@gmail.com>
2024-08-01 14:32:31 +0900

    パーミッションの変更をして、空行・空白を追加。functionをアロー関数に変更して、使っていない変数名の名前を変更。

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+
 const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
-    db.run("INSERT INTO books(title) VALUES('cherry')", function () {
+  () => {
+    db.run("INSERT INTO books (title) VALUES ('cherry')", function () {
       console.log(this.lastID);
 
-      db.run("INSERT INTO books(title) VALUES('blueberry')", function () {
+      db.run("INSERT INTO books (title) VALUES ('blueberry')", function () {
         console.log(this.lastID);
 
-        db.all("SELECT * FROM books", function (err, rows) {
+        db.all("SELECT * FROM books", (unusedErr, rows) => {
           rows.forEach((row) => {
             console.log(row);
           });
 
-          db.run("DROP TABLE books", function () {
+          db.run("DROP TABLE books", () => {
             db.close();
           });
         });
--- 03.asynchronous/error_callback.js
+++ 03.asynchronous/error_callback.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+
 const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
+  () => {
     db.run(
-      "INSERT INTO non_existing_table(title) VALUES('cherry')",
-      function (err) {
+      "INSERT INTO non_existing_table (title) VALUES ('cherry')",
+      (err) => {
         if (err) {
           console.error(err.message);
         }
 
-        db.all("SELECT * FROM non_existing_table", function (err) {
+        db.all("SELECT * FROM non_existing_table", (err) => {
           if (err) {
             console.error(err.message);
           }
 
-          db.run("DROP TABLE books", function () {
+          db.run("DROP TABLE books", () => {
             db.close();
           });
         });
