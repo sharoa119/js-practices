ed3d6b97416f6212586f953335773a67778eccaa
sharoa119 <sau.rin1109@gmail.com>
2024-09-06 11:49:55 +0900

    promiseのエラーありを作成しました。

--- 03.asynchronous/error_promise.js
+++ 03.asynchronous/error_promise.js
@@ -1,53 +1,22 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+
 const db = new sqlite3.Database(":memory:");
 
-createBooksTable()
-  .then(() => insertData())
-  .then(() => closeDatabase())
-  .catch((err) => {
-    console.error(err);
-    closeDatabase();
-  });
-
-function createBooksTable() {
-  const sql =
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-  return run(sql);
-}
-
-function insertData() {
-  return run("INSERT INTO books(title) VALUES('cherry')")
-    .then((lastID) => {
-      console.log(lastID);
-      return run("INSERT INTO non_existing_table(title) VALUES('blueberry')");
-    })
-    .then((lastID) => {
-      console.log(lastID);
-      return all("SELECT * FROM books");
-    })
-    .then((rows) => {
-      rows.forEach((row) => console.log(row));
-    });
-}
-
-function closeDatabase() {
-  db.close();
-}
-
-function run(sql, params = []) {
+const run = (sql, params = []) => {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
       if (err) {
-        return reject(err);
+        reject(err);
+      } else {
+        resolve(this.lastID);
       }
-      resolve(this.lastID);
     });
   });
-}
+};
 
-function all(sql, params = []) {
+const all = (sql, params = []) => {
   return new Promise((resolve, reject) => {
     db.all(sql, params, (err, rows) => {
       if (err) {
@@ -57,4 +26,20 @@ function all(sql, params = []) {
       }
     });
   });
-}
+};
+
+run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => run("INSERT INTO non_existing_table (title) VALUES ('cherry')"))
+  .catch((err) => {
+    console.error(err.message);
+    return all("SELECT * FROM non_existing_table");
+  })
+  .catch((err) => {
+    console.error(err.message);
+    return run("DROP TABLE books");
+  })
+  .finally(() => {
+    db.close();
+  });
