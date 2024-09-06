70004aaaaf0af083bac1ad68cfb23939d612b289
sharoa119 <sau.rin1109@gmail.com>
2024-09-06 11:28:47 +0900

    promiseのエラーなしを作成。

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,50 +1,47 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+
 const db = new sqlite3.Database(":memory:");
 
-createBooksTable()
-  .then(() => insertData())
-  .then(() => closeDatabase())
-  .catch((err) => console.error(err));
-
-function createBooksTable() {
-  const sql =
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-  return run(sql);
-}
-
-function insertData() {
-  return run("INSERT INTO books(title) VALUES('cherry')")
-    .then((lastID) => {
-      console.log(lastID);
-      return run("INSERT INTO books(title) VALUES('blueberry')");
-    })
-    .then((lastID) => {
-      console.log(lastID);
-      return all("SELECT * FROM books");
-    })
-    .then((rows) => {
-      rows.forEach((row) => console.log(row));
-    });
-}
-
-function closeDatabase() {
-  db.close();
-}
-
-function run(sql, params = []) {
+const run = (sql, params = []) => {
   return new Promise((resolve) => {
     db.run(sql, params, function () {
       resolve(this.lastID);
     });
   });
-}
+};
 
-function all(sql, params = []) {
+const all = (sql, params = []) => {
   return new Promise((resolve) => {
-    db.all(sql, params, (err, row) => {
-      resolve(row);
+    db.all(sql, params, (_err, rows) => {
+      resolve(rows);
     });
   });
-}
+};
+
+run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => run("INSERT INTO books (title) VALUES ('cherry')"))
+  .then((lastID) => {
+    console.log(lastID);
+    return run("INSERT INTO books (title) VALUES ('blueberry')");
+  })
+  .then((lastID) => {
+    console.log(lastID);
+    return all("SELECT * FROM books");
+  })
+  .then((rows) => {
+    rows.forEach((row) => {
+      console.log(row);
+    });
+    return run("DROP TABLE books");
+  })
+  .then(() => {
+    db.close();
+  })
+  .catch((err) => {
+    console.error(err.message);
+    db.close();
+  });
