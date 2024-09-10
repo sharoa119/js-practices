cac8843a460a8ed4f2d3f291e48cf6b8dc5dd0e1
sharoa119 <sau.rin1109@gmail.com>
2024-09-10 16:30:09 +0900

    エラーありのコードを共通のファイルを使用したプログラムに修正しました。

--- 03.asynchronous/error_promise.js
+++ 03.asynchronous/error_promise.js
@@ -1,32 +1,6 @@
 #!/usr/bin/env node
 
-import sqlite3 from "sqlite3";
-
-const db = new sqlite3.Database(":memory:");
-
-const run = (sql, params = []) => {
-  return new Promise((resolve, reject) => {
-    db.run(sql, params, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(this.lastID);
-      }
-    });
-  });
-};
-
-const all = (sql, params = []) => {
-  return new Promise((resolve, reject) => {
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  });
-};
+import { run, all, close } from "./dbPromises.js";
 
 run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
@@ -34,12 +8,15 @@ run(
   .then(() => run("INSERT INTO non_existing_table (title) VALUES ('cherry')"))
   .catch((err) => {
     console.error(err.message);
+
     return all("SELECT * FROM non_existing_table");
   })
   .catch((err) => {
     console.error(err.message);
+
     return run("DROP TABLE books");
   })
-  .finally(() => {
-    db.close();
+  .then(() => close())
+  .catch((err) => {
+    console.error(err.message);
   });
