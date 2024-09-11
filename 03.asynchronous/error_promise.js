bf23cd4ebabc8b80188280e238d9f798b3b72389
sharoa119 <sau.rin1109@gmail.com>
2024-09-11 17:33:43 +0900

    dbを引数として渡すプログラムに変更しました。

--- 03.asynchronous/error_promise.js
+++ 03.asynchronous/error_promise.js
@@ -1,22 +1,28 @@
 #!/usr/bin/env node
 
+import sqlite3 from "sqlite3";
 import { run, all, close } from "./db_promise.js";
 
+const db = new sqlite3.Database(":memory:");
+
 run(
+  db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
-  .then(() => run("INSERT INTO non_existing_table (title) VALUES ('cherry')"))
+  .then(() =>
+    run(db, "INSERT INTO non_existing_table (title) VALUES ('cherry')"),
+  )
   .catch((err) => {
     console.error(err.message);
 
-    return all("SELECT * FROM non_existing_table");
+    return all(db, "SELECT * FROM non_existing_table");
   })
   .catch((err) => {
     console.error(err.message);
 
-    return run("DROP TABLE books");
+    return run(db, "DROP TABLE books");
   })
-  .then(() => close())
+  .then(() => close(db))
   .catch((err) => {
     console.error(err.message);
   });
--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
+import sqlite3 from "sqlite3";
 import { run, all, close } from "./db_promise.js";
 
+const db = new sqlite3.Database(":memory:");
+
 run(
+  db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
-  .then(() => run("INSERT INTO books (title) VALUES ('cherry')"))
+  .then(() => run(db, "INSERT INTO books (title) VALUES ('cherry')"))
   .then((result) => {
     console.log(result.lastID);
 
-    return run("INSERT INTO books (title) VALUES ('blueberry')");
+    return run(db, "INSERT INTO books (title) VALUES ('blueberry')");
   })
   .then((result) => {
     console.log(result.lastID);
 
-    return all("SELECT * FROM books");
+    return all(db, "SELECT * FROM books");
   })
   .then((rows) => {
     rows.forEach((row) => {
       console.log(row);
     });
 
-    return run("DROP TABLE books");
+    return run(db, "DROP TABLE books");
   })
-  .then(() => close());
+  .then(() => close(db));
