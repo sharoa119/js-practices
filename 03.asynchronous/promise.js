7d10956acc1d0b610a65f84d7cc09e23ce895bd0
sharoa119 <sau.rin1109@gmail.com>
2024-09-11 11:36:18 +0900

    空行を追加しました

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -8,16 +8,19 @@ run(
   .then(() => run("INSERT INTO books (title) VALUES ('cherry')"))
   .then((result) => {
     console.log(result.lastID);
+
     return run("INSERT INTO books (title) VALUES ('blueberry')");
   })
   .then((result) => {
     console.log(result.lastID);
+
     return all("SELECT * FROM books");
   })
   .then((rows) => {
     rows.forEach((row) => {
       console.log(row);
     });
+
     return run("DROP TABLE books");
   })
   .then(() => close());
