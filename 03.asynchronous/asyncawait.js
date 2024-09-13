10da3b9662c71a26f686201facbfe861f85e1804
sharoa119 <sau.rin1109@gmail.com>
2024-09-13 13:48:58 +0900

    async/awaitのエラーなしプログラムを作成しました

---
+++ 03.asynchronous/asyncawait.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import { run, all, close } from "./db_promise.js";
+
+const db = new sqlite3.Database(":memory:");
+
+async function execute() {
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  const result1 = await run(db, "INSERT INTO books (title) VALUES ('cherry')");
+  console.log(result1.lastID);
+
+  const result2 = await run(
+    db,
+    "INSERT INTO books (title) VALUES ('blueberry')",
+  );
+  console.log(result2.lastID);
+
+  const rows = await all(db, "SELECT * FROM books");
+  rows.forEach((row) => {
+    console.log(row);
+  });
+
+  await run(db, "DROP TABLE books");
+  await close(db);
+}
+
+execute();
