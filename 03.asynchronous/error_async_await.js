71400590def696eea397212d9bae69e4fd66d9fd
sharoa119 <sau.rin1109@gmail.com>
2024-09-17 12:02:56 +0900

    top-level awaitに修正

--- 03.asynchronous/error_async_await.js
+++ 03.asynchronous/error_async_await.js
@@ -5,25 +5,23 @@ import { run, all, close } from "./db_promise.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function execute() {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    await run(db, "INSERT INTO non_existing_table (title) VALUES ('cherry')");
-  } catch (err) {
-    console.error(err.message);
-  }
+try {
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
 
-  try {
-    await all(db, "SELECT * FROM non_existing_table");
-  } catch (err) {
-    console.error(err.message);
-  } finally {
-    await run(db, "DROP TABLE books");
-    await close(db);
-  }
+  await run(db, "INSERT INTO non_existing_table (title) VALUES ('cherry')");
+} catch (err) {
+  console.error(err.message);
 }
 
-execute();
+try {
+  await all(db, "SELECT * FROM non_existing_table");
+} catch (err) {
+  console.error(err.message);
+} finally {
+  await run(db, "DROP TABLE books");
+
+  await close(db);
+}
