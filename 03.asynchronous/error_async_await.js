33ddeb805cd99d33614b0457c3000852ee95a524
sharoa119 <sau.rin1109@gmail.com>
2024-09-17 12:38:40 +0900

    try...catchの内容を修正しました

--- 03.asynchronous/error_async_await.js
+++ 03.asynchronous/error_async_await.js
@@ -5,23 +5,31 @@ import { run, all, close } from "./db_promise.js";
 
 const db = new sqlite3.Database(":memory:");
 
-try {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
+try {
   await run(db, "INSERT INTO non_existing_table (title) VALUES ('cherry')");
 } catch (err) {
-  console.error(err.message);
+  if (err instanceof Error) {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
 }
 
 try {
   await all(db, "SELECT * FROM non_existing_table");
 } catch (err) {
-  console.error(err.message);
-} finally {
-  await run(db, "DROP TABLE books");
-
-  await close(db);
+  if (err instanceof Error) {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
 }
+
+await run(db, "DROP TABLE books");
+
+await close(db);
