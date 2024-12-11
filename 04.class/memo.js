9c6122416ba12c311056384f7983f73ef1837a59
sharoa119 <sau.rin1109@gmail.com>
2024-12-11 14:38:14 +0900

    .js をたす

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { MemoApp } from "./memo_app"; // メインのアプリケーションをインポート
+import { MemoApp } from "./memo_app.js"; // メインのアプリケーションをインポート
 
 // MemoAppのインスタンスを作成し、アプリを開始
 const memo = new MemoApp();
